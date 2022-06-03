e94d8a3cf088d4c0d3fa494ebcb215b1c54b5386
xalidmalik <xalidmelikov@gmail.com>
2022-06-04 00:38:13 +0300

    init 217 contans duplicate

---
+++ 217.contains-duplicate.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=217 lang=typescript
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+  return true;
+}
+// @lc code=end
